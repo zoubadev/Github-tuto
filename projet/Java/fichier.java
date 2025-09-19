e4d242eca149c1e2af605ec3fd27cf3f218e455e
zoubadev <zaineb.adalet@esprit.tn>
2025-09-19 17:48:07 +0100

    code java

---
+++ projet/Java/fichier.java
@@ -0,0 +1,13 @@
+public class ConcatExample {
+    public static void main(String[] args) {
+        // Déclaration des deux chaînes
+        String ch1 = "zaineb ";
+        String ch2 = "adalet";
+        String ch3= "Esprit";
+        // Concaténation avec l'opérateur +
+        String resultat = ch1 + ch2;
+
+        // Affichage du résultat
+        System.out.println("Résultat de la concaténation : " + resultat);
+    }
+}
